af05fd7d7b0b5c561534b1540b1db3596b9f15d1
Iryna Stein <iryna@sensibleweather.com>
2022-03-04 17:49:00 -0700

    added router

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import OtherPage from "./OtherPage";
+import Fib from "./Fib";
 
 function App() {
   return (
+    <BrowserRouter>
+    <Routes>
+      <Route exact path="/" element={Fib}></Route>
+      <Route exact path="/otherpage" element={OtherPage}></Route>
+    </Routes>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     
+     <header className="App-header">
+       <img src={logo} className="App-logo" alt="logo" />
+       <Link className="App-link" to="/">Home</Link>
+       <Link className="App-link" to="otherpage">Other Page</Link>
+       <a
+         className="App-link"
+         href="https://reactjs.org"
+         target="_blank"
+         rel="noopener noreferrer"
+       >
+         Learn React
+       </a>
+     </header>
+   </div>
+   </BrowserRouter>
+      
   );
 }
 
